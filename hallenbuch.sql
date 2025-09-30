2ba47f77322be616538c1d762bc64ef87cd6d1c5
Lutz Risse <163880942+GitHubLutzR@users.noreply.github.com>
2025-09-30 17:05:02 +0200

    Added SQL Files

---
+++ hallenbuch.sql
@@ -0,0 +1,10 @@
+CREATE TABLE hallenbuch (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    datum DATE NOT NULL,
+    von TIME NOT NULL,
+    bis TIME NOT NULL,
+    gruppe VARCHAR(50) NOT NULL,
+    leiter VARCHAR(100) NOT NULL,
+    vermerk TEXT
+);
+
