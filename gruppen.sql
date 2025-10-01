17f14650921e16fe8267aec92979b83263a55660
Lutz Risse <lutz.risse@computacenter.com>
2025-10-01 16:01:09 +0200

    added list_all_goups.php.txt

--- gruppen.sql
+++ gruppen.sql
@@ -1,5 +1,30 @@
-CREATE TABLE gruppen (
+CREATE TABLE hb_gruppen (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
 );
+INSERT INTO hb_gruppen (name) VALUES ('Dacapo');
+INSERT INTO hb_gruppen (name) VALUES ('Eltern-Kind-Turnen');
+INSERT INTO hb_gruppen (name) VALUES ('Folklore');
+INSERT INTO hb_gruppen (name) VALUES ('Gymnastik');
+INSERT INTO hb_gruppen (name) VALUES ('Jugendgarde (JG)');
+INSERT INTO hb_gruppen (name) VALUES ('Kinderturnen');
+INSERT INTO hb_gruppen (name) VALUES ('Konfetti’s');
+INSERT INTO hb_gruppen (name) VALUES ('Leistungsturnen');
+INSERT INTO hb_gruppen (name) VALUES ('Mädchenturnen');
+INSERT INTO hb_gruppen (name) VALUES ('Männerballet (MB)');
+INSERT INTO hb_gruppen (name) VALUES ('Minigarde (MG)');
+INSERT INTO hb_gruppen (name) VALUES ('Moskitos');
+INSERT INTO hb_gruppen (name) VALUES ('Parkour');
+INSERT INTO hb_gruppen (name) VALUES ('Prinzengarde (PG)');
+INSERT INTO hb_gruppen (name) VALUES ('Roadrunners');
+INSERT INTO hb_gruppen (name) VALUES ('Solotanz');
+INSERT INTO hb_gruppen (name) VALUES ('sonstige');
+INSERT INTO hb_gruppen (name) VALUES ('Taka-Tuka-Land (TTL)');
+INSERT INTO hb_gruppen (name) VALUES ('Tanzduo');
+INSERT INTO hb_gruppen (name) VALUES ('Temptation');
+INSERT INTO hb_gruppen (name) VALUES ('Turnschläppscher');
+INSERT INTO hb_gruppen (name) VALUES ('Vivendi');
+INSERT INTO hb_gruppen (name) VALUES ('Zumba');
+INSERT INTO hb_gruppen (name) VALUES ('Zwergengarde (ZG)');
+TRUNCATE TABLE tabellenname;
 
