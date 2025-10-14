65690a6009102cbbca2db16ece41a47b01aadae5
Lutz Risse <lutz.risse@computacenter.com>
2025-10-14 12:14:11 +0200

    Debug Modus 5 in submit

--- submit.php
+++ submit.php
@@ -84,10 +84,10 @@ $maxZukunft = $jetzt + 3600;
 // Validierung
 if ($eintragZeit > $maxZukunft) {
         if ($hesk_settings['debug']) {
-        $Debuginfo = ";EintragZeit=date('Y-m-d H:i:s', $eintragZeit);MaxZukunft=date('Y-m-d H:i:s', $maxZukunft)";
-        header("Location: index.php?rcsubmit=2$Debuginfo");
-        //echo date_default_timezone_get();
-        //sleep(15); 
+            $Debuginfo = ";EintragZeit=" . date('Y-m-d H:i:s', $eintragZeit) . ";MaxZukunft=" . date('Y-m-d H:i:s', $maxZukunft);
+            header("Location: index.php?rcsubmit=2$Debuginfo");
+            exit;
+            //echo date_default_timezone_get();
         }
         header("Location: index.php?rcsubmit=2");
         exit;
