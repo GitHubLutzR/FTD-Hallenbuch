dbee7b291d9ee8cb43054c72cb456589c3a7889d
Lutz Risse <lutz.risse@computacenter.com>
2025-10-14 13:13:08 +0200

    Requiered fields 2

--- index.php
+++ index.php
@@ -62,19 +62,22 @@ $table = $hesk_settings['db_hb_pfix'] . 'gruppen';
 
 <form action="submit.php" method="post">
   <div class="form-row">
-    <label for="datum">Datum:<span class="required-star">*</span></label>
+    <label for="datum">Datum:</label>
     <input type="date" name="datum" id="datum" value="<?= date('Y-m-d') ?>" required>
+    <span class="required-star">*</span>
   </div>
   <div class="form-row">
-    <label for="von">Von:<span class="required-star">*</span></label>
+    <label for="von">Von:</label>
     <input type="time" name="von" id="von" required>
+    <span class="required-star">*</span>
   </div>
   <div class="form-row">
-    <label for="bis">Bis:<span class="required-star">*</span></label>
+    <label for="bis">Bis:</label>
     <input type="time" name="bis" id="bis" required>
+    <span class="required-star">*</span>
   </div>
   <div class="form-row">
-    <label for="gruppe">Gruppe:<span class="required-star">*</span></label>
+    <label for="gruppe">Gruppe:</label>
     <select name="gruppe" id="gruppe" required>
       <option value="">Bitte wählen</option>
       <?php
@@ -85,11 +88,13 @@ $table = $hesk_settings['db_hb_pfix'] . 'gruppen';
       }
       ?>
     </select>
+    <span class="required-star">*</span>
   </div>
 
   <div class="form-row" id="gruppe-sonstige-row" style="display:none;">
-    <label for="gruppe_sonstige">Bitte Gruppe angeben:<span class="required-star">*</span></label>
+    <label for="gruppe_sonstige">Bitte Gruppe angeben:</label>
     <input type="text" name="gruppe_sonstige" id="gruppe_sonstige" maxlength="100">
+    <span class="required-star">*</span>
   </div>
   <script>
   document.getElementById('gruppe').addEventListener('change', function () {
@@ -98,8 +103,9 @@ $table = $hesk_settings['db_hb_pfix'] . 'gruppen';
   });
   </script>
   <div class="form-row">
-    <label for="leiter">Leiter:<span class="required-star">*</span></label>
+    <label for="leiter">Leiter: </label>
     <input type="text" name="leiter" id="leiter" required>
+    <span class="required-star">*</span>
   </div>
 <!-- 
   <div class="form-row">
