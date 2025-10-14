91e16b2db9633d303427c70a66f1360e3f0dba3a
Lutz Risse <lutz.risse@computacenter.com>
2025-10-14 11:18:24 +0200

    Umstieg auf VSCode Copilot 2

--- ignored/copy-php-hb.sh
+++ ignored/copy-php-hb.sh
@@ -11,8 +11,8 @@ dirs=("" "includes" "admin")
 for dir in "${dirs[@]}"; do
   TARGET_DIR="$SOURCE_ROOT/$dir"
   #echo "SCRIPT_DIR: $SCRIPT_DIR, BASH_SOURCE: ${BASH_SOURCE[0]}, REPO_ROOT: $REPO_ROOT, TARGET_DIR: $TARGET_DIR, pwd: $(pwd)"
-  if ls "$TARGET_DIR"/*.php.txt >/dev/null 2>&1; then
-    for file in "$TARGET_DIR"/*.php.txt; do
+  if ls "$TARGET_DIR"/*.php >/dev/null 2>&1; then
+    for file in "$TARGET_DIR"/*.php; do
       SOURCE_FILE=$(echo "$SOURCE_ROOT/$dir/${file##*/}"|sed 's@\/\/@\/@g')
       TARGET_FILE=$(echo "$TARGET_ROOT/$dir/${file##*/}"|sed 's/php.txt/php/g;s@\/\/@\/@g')
       sudo -u $TARGET_USER cp $SOURCE_FILE $TARGET_FILE
