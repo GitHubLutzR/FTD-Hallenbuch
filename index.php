932a83d6ebf3ec8bcdd1eb5a081a4862f45c5826
Lutz Risse <lutz.risse@computacenter.com>
2025-10-16 22:50:17 +0200

    Ordnung geschaffen 4

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 require_once(__DIR__ . '/config.php');
-require_once(__DIR__ . '/header.php');
+#require_once(__DIR__ . '/header.php');
 require_once(__DIR__ . '/include_public.php');
 
 if (isset($_GET['rcsubmit'])) {
