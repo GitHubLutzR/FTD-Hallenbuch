59e9bcf21400e861a86f0d48dd34eb0591095991
Lutz Risse <lutz.risse@computacenter.com>
2025-10-16 22:38:16 +0200

    Ordnung geschaffen 2

--- include.php
+++ include.php
@@ -2,7 +2,7 @@
 session_start();
 
 define('IN_SCRIPT', true);
-require_once(__DIR__ . '/../config.php');
+require_once(__DIR__ . '/config.php');
 $base_url = '/hallenbuch/'; 
 if (empty($_SESSION['user'])) {
     header('Location: ' . $base_url . 'login.php');
