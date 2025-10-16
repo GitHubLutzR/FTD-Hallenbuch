4396e1308bf521d5da64429a0ac3bd3d10b566d4
Lutz Risse <lutz.risse@computacenter.com>
2025-10-16 23:50:57 +0200

    Ordnung geschaffen 6

--- include_public.php
+++ include_public.php
@@ -6,6 +6,8 @@ require_once(__DIR__ . '/config.php');
 $base_url = '/hallenbuch/'; 
 if (empty($_SESSION['user'])) {
     echo "Session User not set";
-        echo "<div style='text-align:right;'><a href='{$base_url}login.php'>🔐 Admin-Login</a></div>";
+    echo "<div style='text-align:right;'><a href='{$base_url}login.php'>🔐 Admin-Login</a></div>";
+} else {
+    echo "<div style='text-align:right;'><a href='{$base_url}logout.php'>⚙logout</a> | <a href='{$base_url}logout.php'>🚪 Logout</a></div>";
 }
 ?>
