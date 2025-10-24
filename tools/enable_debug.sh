257343cfb7e25932c843396913bbcc60ff238262
Lutz Risse <lutz.risse@computacenter.com>
2025-10-24 12:01:32 +0200

    Prüfung des Servers 3

--- tools/enable_debug.sh
+++ tools/enable_debug.sh
@@ -14,16 +14,16 @@ if [ "$1" = "l" ]; then
   TARGET_ROOT="/opt/lampp/apache2/htdocs/hallenbuch"
   if [ ! -d "$TARGET_ROOT" ]; then
     echo "Zielverzeichnis nicht gefunden, auf dem richtigen Server?"
-    #exit 1
+    exit 1
   fi
-  #sed -i "s/\$hesk_settings\['debug'\] = false;/\$hesk_settings['debug'] = true;/" /opt/lampp/apache2/htdocs/hallenbuch/config.php
+  sed -i "s/\$hesk_settings\['debug'\] = false;/\$hesk_settings['debug'] = true;/" /opt/lampp/apache2/htdocs/hallenbuch/config.php
 elif [ "$1" = "o" ]; then
   TARGET_ROOT="$(dirname "$SCRIPT_DIR"|sed 's@git/FTD-Hallenbuch@HESK/hallenbuch@g')"
   if [ "$SRC_REAL" = "$TGT_REAL" ]; then
 	  echo "Fehler beim Aufruf (SOURCE_ROOT und TARGET_ROOT), auf dem richtigen Server?" >&2
-    #exit 1
+    exit 1
   fi
-  #sed -i "s/\$hesk_settings\['debug'\] = false;/\$hesk_settings['debug'] = true;/" /home/risse/HESK/hallenbuch/config.php
+  sed -i "s/\$hesk_settings\['debug'\] = false;/\$hesk_settings['debug'] = true;/" /home/risse/HESK/hallenbuch/config.php
 else
   echo "Aufruf sollte '$0 l' für Lokales Testen, oder '$0 o' für Online-Server sein." >&2
   exit 0
