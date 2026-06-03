85e9def404140bb4fd059f45898a9fd0758f506d
Lutz Risse <lutz.risse@computacenter.com>
2026-06-03 10:40:06 +0200

    created TestScript

--- tools/test_hallenbuch_online.sh
+++ tools/test_hallenbuch_online.sh
@@ -1,1 +1,47 @@
+#!/bin/bash
 #
+##if [ "$(id -u)" -ne 0 ]; then
+##  if command -v sudo >/dev/null 2>&1; then
+##    exec sudo bash "$0" "$@"
+##  else
+##    echo "Dieses Script benötigt root-Rechte, aber sudo ist nicht verfügbar." >&2
+##    exit 1
+##  fi
+##fi
+# kopiert die php.txt Dateien vom Repo ins html-Verzeichnis und benennt sie als php Dateien.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SOURCE_ROOT="$(dirname "$SCRIPT_DIR")"
+ONLINE_DIR="/home/risse/HESK/hallenbuch"
+dirs=("" "includes" "admin")
+
+#if [ "$1" = "l" ]; then
+#  TARGET_ROOT="/opt/lampp/apache2/htdocs/hallenbuch"
+#  if [ ! -d "$TARGET_ROOT" ]; then
+#    echo "Zielverzeichnis nicht gefunden, auf dem richtigen Server?"
+#    exit 1
+#  fi
+#  TARGET_USER="root:daemon"
+#elif [ "$1" = "o" ]; then
+  TARGET_ROOT=${ONLINE_DIR}
+#  if [ "$SOURCE_ROOT" = "$TARGET_ROOT" ]; then
+#	  echo "Fehler beim Aufruf (SOURCE_ROOT und TARGET_ROOT), auf dem richtigen Server?" >&2
+#    exit 1
+#  fi  
+  TARGET_USER="www-data:"
+#else
+#  echo "Aufruf sollte '$0 l' für Lokales Testen, oder '$0 o' für Online-Server sein." >&2
+#  exit 0
+#fi
+scp $SOURCE_ROOT/includes/view_entries-n.php 217.160.3.116:/home/risse/HESK/hallenbuch/includes/view_entries.php
+exit 0
+for dir in "${dirs[@]}"; do
+  if ls "$SOURCE_ROOT/$dir"/*.php >/dev/null 2>&1; then
+      SOURCE_FILE=$(echo "$SOURCE_ROOT/$dir/${file##*/}"|sed 's@\/\/@\/@g')
+      echo "########## $SOURCE_ROOT/$dir ##########"
+      ls -l $SOURCE_ROOT/$dir/*.php
+  fi
+done
+
+#chown -R $TARGET_USER $TARGET_ROOT
+
+exit 0
